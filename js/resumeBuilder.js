690d9ddaac0d3a71c17471cc2878ddb7d07c8d8c
taban@tabancosmos.com <taban@tabancosmos.com>
2015-03-18 20:14:24 -0700

    added new items

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,3 +2,14 @@
  * Created by Taban on 3/18/15.
  */
 $("#main").append(["Taban Cosmos"]);
+
+//using the replace method.
+
+var email = "taban@gmail.com";
+
+var newEmail = email.replace("gmail", "tabancosmos");
+
+//log the original email and the old email to console
+
+console.log(email);
+console.log(newEmail);
