68f0363a3a94b9639b8bb34d7c342213dbd1562c
taban@tabancosmos.com <taban@tabancosmos.com>
2015-03-19 21:35:31 -0700

    updates

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -13,11 +13,19 @@
 //console.log(newEmail);
 var name = "Taban Cosmos";
 var role = "Developer";
+var phoneNumber = "(425)-209-8052";
+var email = "taban@tabancosmos.com";
+var github = "https://github.com/Cosmos-it";
 
 var formattedName = HTMLheaderName.replace("%data%", name);
 var formattedRole = HTMLheaderRole.replace("%data%", role);
+var formattedPhone = HTMLcontactGeneric.replace("%data%", phoneNumber);
+var formattedEmail = HTMLcontactGeneric.replace("%contact%", email);
 
 $("#header").prepend(formattedName);
 $("#header").prepend(formattedRole);
+$("#topContacts").append(formattedPhone);
+$("#topContacts").append(formattedEmail);
+
 
 
